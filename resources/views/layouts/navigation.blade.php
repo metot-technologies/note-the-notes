d5a910f491e65bf6f9e5a6c51204b498079daa82
gedemartha <alphamart12@gmail.com>
2023-10-23 12:58:02 +0800

    style: note-cards and metot logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('main') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <img src="{{ URL::to('/') }}/images/metot-removebg-preview.png" alt="" width=75px height=75px>
                     </a>
                 </div>
 
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-white leading-tight ">
             {{ __('Home') }}
         </h2>
     </x-slot>
@@ -10,24 +10,30 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <a href="{{ route('note.create')}}"
-                       class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Create</a>
+                       class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Create your note !</a>
                     @error('deleteError')
                         <ul class='text-sm text-red-600 space-y-1'>
                             <li>{{ $message }}</li>
                         </ul>
                     @enderror
-                    <h2 class="mt-5">Note: </h2>
+                    <h2 class="mt-5 ">Note(s): </h2>
                     @if($notes->first)
-                        @foreach ($notes as $note)
-                            <p><b>Title:</b> {{$note['title']}}</p>
-                            <a href="{{ route('note.edit', $note['id']) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Detail</a>
-                            <form action="{{route('note.destroy', $note['id'])}}" method="post">
-                                @csrf
-                                @method('delete')
-                                <button type="submit">Delete</button>
-                            </form>
-                            <p>-------------------</p>
-                        @endforeach
+                    <div class="grid grid-cols-4 gap-4">
+                        
+                            @foreach ($notes as $note)
+                            <div class="p-6 border-2 border-rose-500 w-[100%]">
+                                <p><b>Title:</b> {{$note['title']}}</p>
+                                <div class="mt-10 flex gap-x-5">
+                                    <a href="{{ route('note.edit', $note['id']) }}" class="border-2 border-blue-800 font-bold text-blue-600 dark:text-blue-500 hover:underline">Detail</a>
+                                    <form action="{{route('note.destroy', $note['id'])}}" method="post">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="text-red-600 font-bold border-2 border-rose-500 hover:underline">Delete</button>
+                                    </form>
+                                </div>
+                            </div>    
+                            @endforeach
+                    </div>
                     @endif
                 </div>
             </div>
